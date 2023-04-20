c9367392211c7dfa2392e59777499221b57c1b76
Thomas Schmidt <tschmidt@suse.de>
2023-04-20 14:01:08 +0200

    improve prompt

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -12,20 +12,20 @@ class Answer
     # Note: limiting the article text because of max context length of 4096 tokens (incl. the 400 from the response)
     article_context = @article.text.split[0..900].join(' ')
 
-    # Note: adding a '?' after the input, so the 'completions' endpoint doesn't try to
-    # complete the question.
-     Rails.logger.info "Article has #{@article.text.split.size} words"
+    Rails.logger.info "Article has #{@article.text.split.size} words, using 900."
     prompt =
-    "You are an AI assistant. You work for the SUSE Customer Center team at SUSE which is a open source operating system.
-    You will be asked questions from a customer and will answer in a helpful and friendly manner.
-    You will be provided information from SUSE under the [Article] section. The customer question
-    will be provided under the [Question] section. You will answer the customers questions only based on information
-    from the article.
-    If the users question is not answered by the article you will respond with 'I'm sorry Dave, I don't know.'
+    "You are an AI assistant answering customer requests based on performing a
+    semantic search over SUSE documentation and knowledge base articles.
+    You will answer in a helpful and friendly manner.
+    You will be provided information from SUSE under the [Article] section.
+    The customer request will be provided under the [Question] section.
+    You will answer the customer's requests only based on information from the article.
+    If the customer's request is not answered by the article you will respond with
+    'I'm sorry Dave, I don't know.'
     [Article]
     #{article_context}
     [Question]
-    #{@question.question}?"
+    #{@question.question}"
     
 		openai = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
     # rate limits: https://platform.openai.com/docs/guides/rate-limits/overview
@@ -42,7 +42,8 @@ class Answer
       Rails.logger.error(response.inspect)
       raise Exception.new("Generating answer failed: " + response['error'].inspect)
     end
-    response['choices'][0]['text'].lstrip.sub('Answer: ', '')
+    # strip punctuation from the beginning of the string if it was completed.
+    response['choices'][0]['text'].sub(/^[ ?!\.]*/, '')
   end
 
 end
