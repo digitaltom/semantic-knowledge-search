f7b9fb4346118196a91041d69e4c24d905b4d866
Thomas Schmidt <tschmidt@suse.de>
2023-06-15 00:59:40 +0200

    show top article, index man pages

--- db/migrate/20230505153133_create_vss_table.rb
+++ db/migrate/20230505153133_create_vss_table.rb
@@ -1,4 +1,7 @@
+require 'vss0'
+
 class CreateVssTable < ActiveRecord::Migration[7.1]
+  ensure_vss0
   def up
     ActiveRecord::Base.connection.execute('CREATE VIRTUAL TABLE IF NOT EXISTS vss_articles using vss0(embedding(1536))')
   end
